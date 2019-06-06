033fc4261632f26b8665de83eadb5ffee0c9d652
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-06-06 17:23:07 +0600

    fix js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let clock = document.querySelector(".clock div");
 let timeLine = 0;
 
 function time() {
-  timeLine += 1;
+  timeLine === 360 ? (timeLine = 0) : (timeLine += 1);
   clock.style.transform = `rotateZ(${timeLine}deg)`;
 
   let timeOut = setTimeout(() => {
